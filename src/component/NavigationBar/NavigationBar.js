2b5e08346b212509a5c13c08d4a5d83893e60268
MSI <chathurabhashitha01@gmail.com>
2024-03-21 01:23:56 +0530

    add buttons

--- src/component/NavigationBar/NavigationBar.js
+++ src/component/NavigationBar/NavigationBar.js
@@ -9,7 +9,7 @@ export default function NavigationBar() {
     <div>
          <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">React-Bootstrap-Example</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
--- src/page/InputForm/InputForm.js
+++ src/page/InputForm/InputForm.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
 import NavigationBar from '../../component/NavigationBar/NavigationBar';
+import Button from 'react-bootstrap/Button';
+import './style.css'
+import { ButtonGroup } from 'react-bootstrap';
 
 export default function InputForm() {
   return (
     <>
-    <Form>
+    <Form className='frmBody'>
         <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="title" placeholder="Enter title" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Body</Form.Label>
+        <Form.Control type="body" placeholder="Enter body" />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>UserID</Form.Label>
+        <Form.Control type="userID" placeholder="Enter UserID" />
+        </Form.Group>
+
+    <ButtonGroup>
+    <Button variant="outline-primary">SAVE</Button>{' '}
+      <Button variant="outline-success">UPDATE</Button>{' '}
+      <Button variant="outline-warning">DELETE</Button>{' '}
+      </ButtonGroup>
     </Form>
   </>
   )
