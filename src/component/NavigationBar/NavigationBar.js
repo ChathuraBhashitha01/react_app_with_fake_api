428e5cf96830c1c85d324afaba0f30ab03e2d519
MSI <chathurabhashitha01@gmail.com>
2024-03-21 00:40:17 +0530

    Input  page creation

--- src/component/NavigationBar/NavigationBar.js
+++ src/component/NavigationBar/NavigationBar.js
@@ -2,19 +2,23 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export default function NavigationBar() {
   return (
-    <>
-         <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <div>
+         <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Link className='nav-link' >Home</Link>
+                    <Link className='nav-link' >About</Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </div>
   )
 }
