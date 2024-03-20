ea43734dee6d0f897c3c177b685badd0894fc6e8
MSI <chathurabhashitha01@gmail.com>
2024-03-21 02:27:42 +0530

    Update ,delete funtion creation

--- src/page/InputForm/InputForm.js
+++ src/page/InputForm/InputForm.js
@@ -26,6 +26,28 @@ export default function InputForm() {
         .then((json) => console.log(json))
         .catch((err)=>{})
     }
+    const updateData=()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: 1,
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }
+
+    const deleteData=()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+        });
+    }
 
   return (
     <>
